458a38291a1c0753b9b2712b4a121209ada9304c
Marta Martínez <92877884+martroch@users.noreply.github.com>
2022-04-23 20:49:22 +0200

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,11 @@
+<?php $title="login";?>
+<?php $path="../";?>
+<?php $active="area-de-usuario";?>
+<?php include($path . 'php/head.php')?>
+<body>
+    <?php include($path . 'php/nav.php')?>
+
+    <?php include($path . 'php/footer.php')?>
+</body>
+
+</html>
